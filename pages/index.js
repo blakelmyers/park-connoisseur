715fd736f253ffe2e86c050fc6dba7aec988cf1d
Blake Myers <blakelmyers@gmail.com>
2022-06-05 22:17:16 -0500

    First Commmit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import Banner from '../components/banner'
+
+export default function Home() {
+
+  const handleOnBannerBtnClick = () => {
+     console.log('Banner Button');
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Park Connoisseur</title>
+        <meta name="description" content="Park Locator App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+        </h1>
+        <Banner buttonText="View Parks Nearby" 
+        handleOnClick={handleOnBannerBtnClick}/>
+      </main>
+    </div>
+  )
+}
