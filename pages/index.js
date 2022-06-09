e74cdecd246b8515a0bff7de14f5282c9674f902
Blake Myers <blakelmyers@gmail.com>
2022-06-08 23:59:08 -0500

    add card component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import Banner from '../components/banner'
+import Card from "../components/card";
+
+import parkLocations from "../data/park-location.json"
 
 export default function Home() {
 
@@ -31,6 +34,19 @@ export default function Home() {
             alt="hero image"
           />
         </div>
+      <div className={styles.cardLayout}>
+        {parkLocations.map((parkInfo) => {
+          return (
+            <Card
+               className={styles.card}
+               key={1}
+               name={parkInfo.name}
+               imgUrl={parkInfo.imgUrl}
+               href={`/park-info/${parkInfo.name}`}
+            />
+          );
+        })}
+       </div>
       </main>
     </div>
   )
